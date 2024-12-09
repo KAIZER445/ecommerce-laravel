34b563f72e5333551fc445fccf544b2e12756dcc
KAIZER445 <prashanttamang861@gmail.com>
2024-12-09 20:24:04 +0545

    include in routing complete

--- routes/backend/backend.php
+++ routes/backend/backend.php
@@ -1,11 +1,10 @@
 <?php
 
+use App\Http\Controllers\Backend\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>'ecommerce-backend'], function (){
-    Route::get('/',function(){
-        return 'i am backend';
-    });
+    Route::get('/',[DashboardController::class,'index']);
 
     require_once __DIR__ . '\user\user.php';
 });

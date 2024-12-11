161ed18d928a9abaf0ff1e3e7feb6d63ae9eb8e1
KAIZER445 <prashanttamang861@gmail.com>
2024-12-11 21:28:46 +0545

    laravel breeze ui installation

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return 'hello';
+    return view('welcome');
 });
 
-require_once __DIR__ . '/backend/backend.php';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

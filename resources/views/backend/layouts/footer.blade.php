23d654f0d761954515285e6cff5c2474e47bdc7f
KAIZER445 <prashanttamang861@gmail.com>
2024-12-11 20:33:25 +0545

    fornt end template updated

---
+++ resources/views/backend/layouts/footer.blade.php
@@ -0,0 +1,8 @@
+@section('footer')
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <script src="{{url('backend-assets/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{url('backend-assets/assets/js/main.js')}}"></script>
+
+</body>
+</html>
+@endsection

96831d37b72a648e31fd9cd0eaf27adcbb0cc73b
KAIZER445 <prashanttamang861@gmail.com>
2024-12-12 21:52:03 +0545

    git login and registration complete

--- routes/backend/backend.php
+++ routes/backend/backend.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Backend\Dashboard\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>'ecommerce-backend'], function (){
-    Route::get('/',[DashboardController::class,'index']);
+    Route::get('/',[DashboardController::class,'index'])->name('ecommerce-dashboard');
 
     require_once __DIR__ . '\user\user.php';
 });
